8628ba1b301460081ae124f45e0ed5394c7b6f3a
coder Su <457324551@qq.com>
2017-07-27 15:27:22 +0800

    项目结构调整

---
+++ QFTV/Classes/Home/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  QFTV
+//
+//  Created by SuBo on 2017/7/8.
+//  Copyright © 2017年 SuBo. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor=UIColor.white
+    }
+}
---
+++ QFTV/Classes/Main/Controller/NavigationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewController.swift
+//  QFTV
+//
+//  Created by SuBo on 2017/7/8.
+//  Copyright © 2017年 SuBo. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.barTintColor=UIColor.black
+    }
+}
